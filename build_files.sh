36c772587fe69f9f4514a77327c88fdc25b919c9
shivamkapoor172002 <shivamkapoor2002@gmail.com>
2026-02-22 09:41:53 +0530

    Fix build script for Vercel deployment

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# Exit on error
+set -e
+
 echo "Building project..."
-python3.12 -m pip install -r requirements.txt
+# Use python3 instead of python3.12 to be safer with paths
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
 
 echo "Collecting static files..."
-python3.12 manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput --clear
 
 echo "Build complete."
